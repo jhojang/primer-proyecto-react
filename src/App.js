ac4d3f7eb326f8a7fa05c85df0e472b26d72da2f
johang <jhojan.gomez4@gmail.com>
2020-06-26 23:33:38 -0500

    Cards hechas

--- src/App.js
+++ src/App.js
@@ -3,6 +3,33 @@ import logo from './logo.svg';
 import './App.css';
 import Card from './Card.jsx';
 
+const cards = [
+  {
+
+  },
+  {
+    imagen: "https://programacion.net/files/article/article_01934_.png",
+    avatar: "https://img.pngio.com/avatar-icon-png-105-images-in-collection-page-3-avatarpng-512_512.png",
+    titulo: "Título de card número 1",
+    autor: "Alexis Jaramillo",
+    boton: "$25"
+  },
+  {
+    imagen: "https://postparaprogramadores.com/wp-content/uploads/2020/04/44-min.png",
+    avatar: "https://www.w3schools.com/howto/img_avatar.png",
+    titulo:"Título de card número 2",
+    autor: "Alexis Jaramillo",
+    boton: "$30"
+  },
+  {
+    imagen: "https://codigoonclick.com/wp-content/uploads/2018/03/Conoce-Angular-1080x630.jpg",
+    avatar: "https://monteirolobato.com.br/public/assets/admin/images/avatars/avatar2_big.png",
+    titulo:"Título de card número 2",
+    autor: "Roberto Lopez",
+    boton: "$50"
+  }
+];
+
 function App() {
   return (
     <>
@@ -10,9 +37,8 @@ function App() {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Había una vez una iguana con una ruana de lana
+            Hola mundo con React JS
           </p>
-          <a className="App-link" href="">Enlace</a>
           <a
             className="App-link"
             href="https://reactjs.org"
@@ -24,12 +50,25 @@ function App() {
         </header>
       </div>
       <div className="card-container">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        {
+          /*cards.map(function(card) {
+            return <Card
+                    imagen = {card.imagen}
+                    avatar = {card.avatar}
+                    titulo = {card.titulo}
+                    autor = {card.autor}
+                    boton = {card.boton}
+                  />
+          })*/
+
+          cards.map(card => <Card 
+                              imagen = {card.imagen}
+                              avatar = {card.avatar}
+                              titulo = {card.titulo}
+                              autor = {card.autor}
+                              boton = {card.boton}
+                            />)
+        }
       </div>
     </>
   );
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,26 +1,41 @@
 import React from 'react';
 import './Card.css';
-import ReactImg from './REACTjs.jpg';
-import avatarImg from './avatar-img.jpg';
+import PropTypes from "prop-types"
 
-function Card() {
+function Card({imagen, titulo, autor, avatar, boton}) {
     return (
         <div className="card">
             <div className="img-container">
-                <img src={ReactImg} alt=""/>
+                <img src={imagen} alt=""/>
             </div>
             <div className="card-description">
-                <h3>Aquí va el título de la card</h3>
+                <h3>{titulo}</h3>
                 <div className="autor-content">
-                    <img src={avatarImg} alt=""/>
-                    <span>Nombre del autor</span>
+                    <img src={avatar} alt=""/>
+                    <span>{autor}</span>
                 </div>
                 <div className="link">
-                    <a href="">Iniciar</a>
+                    <a href="">{boton}</a>
                 </div>
             </div>
         </div>
     )
+}
+
+Card.propTypes = {
+    imagen: PropTypes.string,
+    titulo: PropTypes.string,
+    avatar: PropTypes.string,
+    autor: PropTypes.string,
+    boton: PropTypes.string
+}
+
+Card.defaultProps = {
+    imagen: "https://farm5.staticflickr.com/4363/36346283311_1dec5bb2c2.jpg",
+    titulo: "---",
+    avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTmItPhy3D1zdB1ufVECWplIEtUTQ_XPvtoTw&usqp=CAU",
+    autor: "---",
+    boton: "$0"
 }
 
 export default Card;
